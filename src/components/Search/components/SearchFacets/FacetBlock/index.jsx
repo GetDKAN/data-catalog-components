6efd42426caa2009ece58584279589ff2cd8d597
Daniel Gading <dgading@gmail.com>
2019-09-20 13:13:30 -0400

    Adds toggle block with tests

---
+++ src/components/Search/components/SearchFacets/FacetBlock/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const FacetBlock = ({
+  title,
+  children,
+  headingClasses,
+  innerClasses,
+  allowToggle
+}) => {
+  const [showFacets, toggleShowFacets] = useState(true);
+  let facetBlockHeading = <h2 className={headingClasses}>{title}</h2>
+  if(allowToggle) {
+    facetBlockHeading = 
+      (<h2 className={headingClasses}>
+        <button onClick={() => toggleShowFacets(!showFacets)}>
+          {title}
+        </button>
+      </h2>)
+    
+  }
+  return(
+    <section>
+      {facetBlockHeading}
+      {showFacets &&
+        <div className={innerClasses}>
+          {children}
+        </div>
+      }
+    </section>
+  );
+}
+
+FacetBlock.defaultProps = {
+  allowToggle: true,
+  headingClasses: 'dkan-facet-block-title',
+  innerClasses: 'dkan-facet-block-inner'
+}
+
+FacetBlock.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  headingClasses: PropTypes.string,
+  innerClasses: PropTypes.string,
+  allowToggle: PropTypes.bool
+}
---
+++ src/components/Search/components/SearchFacets/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FacetBlock } from './FacetBlock';
+
+const SearchFacets = ({}) => {
+  return(
+    <FacetBlock>
+      <h1>blajdaif</h1>
+    </FacetBlock>
+  );
+}
+
+SearchFacets.defaultProps = {};
+SearchFacets.propTypes = {};
+
+export default SearchFacets;
