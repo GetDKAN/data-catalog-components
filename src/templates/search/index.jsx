ab17ece734d32837721aea29e6e216ab958870dd
fmizzell <fmizzell.dev@gmail.com>
2020-01-16 09:28:44 -0600

    Consistency.

--- src/templates/search/index.jsx
+++ src/templates/search/index.jsx
@@ -59,7 +59,7 @@ const Search = () => {
   return (
     <SearchDispatch.Provider value={{ searchState, dispatch }}>
         <SearchInput
-            onChangeFunction={(e) =>
+            onChangeFunction={e =>
                 dispatch({ type: "UPDATE_QUERY", data: { query: e.target.value } })
             }
             onResetFunction={() => dispatch({ type: "RESET_QUERY" })}
@@ -88,8 +88,8 @@ const Search = () => {
           itemsCountPerPage={pageSize}
           totalItemsCount={totalItems}
           pageRangeDisplayed={5}
-          onChange={event =>
-            dispatch({ type: "UPDATE_CURRENT_PAGE", data: { page: event } })
+          onChange={e =>
+            dispatch({ type: "UPDATE_CURRENT_PAGE", data: { page: e } })
           }
         />
     </SearchDispatch.Provider>
