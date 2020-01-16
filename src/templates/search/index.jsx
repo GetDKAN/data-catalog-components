d745ab5b8bbc36abda172857f9b13d87df90b1d9
fmizzell <fmizzell.dev@gmail.com>
2020-01-16 09:08:40 -0600

    Adding pagination to search template.

--- src/templates/search/index.jsx
+++ src/templates/search/index.jsx
@@ -1,39 +1,48 @@
 import React, { useEffect, useReducer } from "react";
 import SearchListItem from "../../components/SearchListItem";
 import SearchInput from "../../components/SearchInput";
+import Pagination from "react-js-pagination";
 import { searchReducer, defaultSearchState, SearchDispatch } from "../../services/search/search_tools";
 
 const Search = () => {
 
+  defaultSearchState.pageSize = 2;
   // Initialize the useReducer hook using the imported searchState and reducer.
   const [searchState, dispatch] = useReducer(searchReducer, defaultSearchState);
-  const { items, query } = searchState;
+  const { items, totalItems, query, page, pageSize } = searchState;
 
   // Fetch data on mount and when query changes.
   useEffect(() => {
-    dispatch({
-        type: 'GET_SEARCH_DATA',
-        data: {
-            totalItems: 10,
-            items: [{
-                identifier: 1,
-                title: "I am the title",
-                description: "And this is the description."
-            },
-            {
-                identifier: 2,
-                title: query,
-                description: "And this is the description 2."
-            }],
-            facetsResults: {}
-    }});
+    let info = getData(query, page, pageSize);
     
-  }, [query]);
+    dispatch({
+      type: 'GET_SEARCH_DATA',
+      data: {
+        totalItems: info.total,
+        items: info.items,
+        facetsResults: {}
+      }});
 
-  async function getData(query) {
-    const axios = require('axios').default;
-    const response = await axios.get('http://dkan/api/1/search?fulltext=' + query);
-    return response.data;
+  }, [query, page, pageSize]);
+
+  function getData(query, page, pageSize) {
+    let items = [
+      {title: "Hello", description: "Goodbye"},
+      {title: "Hola", description: "Chao"},
+      {title: "Bonjour", description: "Au revoir"},
+    ]
+
+    if (query) {
+      items = items.filter((item) => {
+        return (item.title == query || item.description == query)
+      });
+    }
+
+    const total = items.length;
+
+    const start = (page * pageSize) - pageSize;
+    items = items.splice(start, pageSize);
+    return {total: total, items: items};
   }
 
   return (
@@ -52,6 +61,16 @@ const Search = () => {
             <SearchListItem key={item.identifier} item={item} />
             ))}
         </ol>
+        <Pagination
+          hideDisabled
+          activePage={page}
+          itemsCountPerPage={pageSize}
+          totalItemsCount={totalItems}
+          pageRangeDisplayed={5}
+          onChange={event =>
+            dispatch({ type: "UPDATE_CURRENT_PAGE", data: { page: event } })
+          }
+        />
     </SearchDispatch.Provider>
   );
 };
