914fd1151eb2ee56dd73d930ea1c13954d20b618
Daniel Gading <dgading@gmail.com>
2020-04-01 12:51:12 -0400

    Initial Modal component

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AriaModal from 'react-aria-modal';
+
+const Modal = ({
+  nodeId, children, closeText, openText, title,
+}) => {
+  const [modalOpen, setModalOpen] = React.useState(false);
+  const getNode = () => document.getElementById(nodeId);
+  const titleId = title.replace(/\s/g, '_').toLowerCase();
+  const popup = modalOpen
+    ? (
+      <AriaModal
+        titleText={title}
+        getApplicationNode={getNode}
+        initialFocus={`#dc-modal-${titleId}-close`}
+      >
+        <div
+          id={`dc-modal-${titleId}`}
+        >
+          <header className="dc-modal-header">
+            <h2>{title}</h2>
+            <button
+              type="button"
+              id={`dc-modal-${titleId}-header-close`}
+              onClick={() => setModalOpen(false)}
+              className="dc-modal-close-button"
+            >
+              {closeText}
+            </button>
+          </header>
+          <div className="dc-modal-body">
+            {children}
+          </div>
+          <footer className="dc-modal-footer">
+            <button
+              type="button"
+              id={`dc-modal-${titleId}-close`}
+              onClick={() => setModalOpen(false)}
+              className="dc-modal-close-button"
+            >
+              {closeText}
+            </button>
+          </footer>
+        </div>
+      </AriaModal>
+    )
+    : false;
+
+  return (
+    <div className="dc-modal">
+      <button
+        className="dc-modal-open-button"
+        id={`dc-modal-${titleId}-open`}
+        type="button"
+        onClick={() => setModalOpen(!modalOpen)}
+      >
+        {openText}
+      </button>
+      {popup}
+    </div>
+  );
+};
+
+Modal.defaultProps = {
+  closeText: 'Close Modal',
+  openText: 'Open Modal',
+};
+
+Modal.propTypes = {
+  title: PropTypes.string.isRequired,
+  closeText: PropTypes.string,
+  openText: PropTypes.string,
+  nodeId: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Modal;
