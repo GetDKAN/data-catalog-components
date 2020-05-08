596cece2ddd87382e2ac01af15edc676e4d48924
Daniel Gading <dgading@gmail.com>
2020-05-08 14:44:12 -0400

    Initial docz build

---
+++ doczrc.js
@@ -0,0 +1,21 @@
+
+export default {
+  title: 'Data Catalog Components',
+  description: 'A component library for data catalogs.',
+  src: './src', // Where you want docz to look for MDX files
+  dest: './docs', // Where you want the output placed.
+  // base: '/form-components/', // This is added so I could use GitHub pages.
+  // typescript: true,
+  themeConfig: {
+    showPlaygroundEditor: true,
+  },
+  // menu: [
+  //   'Home',
+  //   {
+  //     name: 'Components',
+  //     menu: [
+  //       'Tags',
+  //     ],
+  //   },
+  // ],
+};
