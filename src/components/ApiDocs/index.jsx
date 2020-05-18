e3bec5ed49f669b51eff0c54b798803db86f6e6f
Janette Day <janette.day@civicactions.com>
2020-05-18 11:34:29 -0500

    wip

--- src/components/ApiDocs/index.jsx
+++ src/components/ApiDocs/index.jsx
@@ -1,11 +1,10 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css";
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
 
-const ApiDocs = ({endpoint, uuid}) => {
-
-  const url = uuid ? endpoint + "/" + uuid : endpoint;
+const ApiDocs = ({ endpoint, uuid }) => {
+  const url = uuid ? `${endpoint}/${uuid}` : endpoint;
 
   return (
     <SwaggerUI url={url} docExpansion="list" />
--- src/components/IconList/index.jsx
+++ src/components/IconList/index.jsx
@@ -1,60 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function IconList(props) {
-  const ComponentToRender = props.component;
-  let content = (<div></div>);
-  let styles = {
-    textAlign: props.titleAlign
+function IconList(items, component, containerClass, listClass, paneTitle, titleAlign) {
+  const ComponentToRender = component;
+  let content = (<div />);
+  const styles = {
+    textAlign: titleAlign,
   };
-  
+
   // If we have items, render them
-  if (props.items) {
-      content = props.items.map((item, i) => (
-          <ComponentToRender key={i} 
-            title={item.title} 
-            image={item.image}
-            link={item.ref}
-            color={item.color}
-            size={item.size}
-            index={i}
-          />
-      ));
+  if (items) {
+    content = items.map((item, i) => (
+      <ComponentToRender
+        key={i}
+        title={item.title}
+        image={item.image}
+        link={item.ref}
+        color={item.color}
+        size={item.size}
+        index={i}
+      />
+    ));
   } else {
-      // Otherwise render a single component
-      content = (<ComponentToRender />);
+    // Otherwise render a single component
+    content = (<ComponentToRender />);
   }
 
-  if (props.paneTitle) {
+  if (paneTitle) {
     return (
-      <div className={`dc-icon-list  {$props.containerClass}`}>
-        <h2 className="pane-title" style={styles}>{ props.paneTitle }</h2>
-        <ul className={ props.listClass }>
+      <div className={'dc-icon-list  {containerClass}'}>
+        <h2 className="pane-title" style={styles}>{ paneTitle }</h2>
+        <ul className={listClass}>
           {content}
         </ul>
       </div>
     );
   }
-  else {
-    return (
-      <div className={`dc-icon-list  {$props.containerClass}`}>
-        <ul className={ props.listClass }>
-          {content}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div className={'dc-icon-list  {containerClass}'}>
+      <ul className={listClass}>
+        {content}
+      </ul>
+    </div>
+  );
 }
 
 IconList.defaultProps = {
+  ComponentToRender: 'IconListItem',
   items: [],
-  className: "icon-list",
-  paneTitle: "Icon List"
+  listClass: 'dc-list',
+  containerClass: 'container',
+  paneTitle: '',
+  titleAlign: '',
 };
 
 IconList.propTypes = {
-  component: PropTypes.func.isRequired,
-  items: PropTypes.array,
+  ComponentToRender: PropTypes.string,
+  items: PropTypes.arrayOf,
   listClass: PropTypes.string,
   containerClass: PropTypes.string,
   paneTitle: PropTypes.string,
