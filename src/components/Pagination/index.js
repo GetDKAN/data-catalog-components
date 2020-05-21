f7514a1031807340f01cf3cc05ffa3c761d18bfa
fmizzell <fmizzell.dev@gmail.com>
2020-05-20 20:11:54 -0500

    Remove unused components.

--- src/components/Pagination/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-
-import React from "react";
-import { Pagination } from "reactstrap";
-import { PaginationItem } from "reactstrap";
-import { PaginationLink } from "reactstrap";
-
-export default Pagination;
--- src/services/context/ResourceContext.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ResourceContext = React.createContext();
-
-export default ResourceContext;
