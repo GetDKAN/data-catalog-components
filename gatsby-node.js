3f4bd3517811a03b04d09e410d99e121f192458a
Daniel Gading <dgading@gmail.com>
2020-05-21 15:15:46 -0400

    Add module export for null loader on swagger ui in docz

---
+++ gatsby-node.js
@@ -0,0 +1,22 @@
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html' || stage === 'develop-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          // {
+          //   test: /react-loader-advanced/,
+          //   use: loaders.null(),
+          // },
+          // {
+          //   test: /react-loading-spin/,
+          //   use: loaders.null(),
+          // },
+          {
+            test: /swagger-ui-react/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
+};
