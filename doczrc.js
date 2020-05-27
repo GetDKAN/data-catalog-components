67af49885d47682bbf0a912e4dc884da3a711330
Daniel Gading <dgading@gmail.com>
2020-05-27 12:23:10 -0400

    Add home to docz

--- doczrc.js
+++ doczrc.js
@@ -4,19 +4,14 @@ export default {
   description: 'A component library for data catalogs.',
   src: './src', // Where you want docz to look for MDX files
   dest: './docs', // Where you want the output placed.
-  // base: '/form-components/', // This is added so I could use GitHub pages.
-  // typescript: true,
   themeConfig: {
     showPlaygroundEditor: true,
   },
   base: '/data-catalog-components',
-  // menu: [
-  //   'Home',
-  //   {
-  //     name: 'Components',
-  //     menu: [
-  //       'Tags',
-  //     ],
-  //   },
-  // ],
+  menu: [
+    'Home',
+    'Components',
+    'Services',
+    'Templates',
+  ],
 };
