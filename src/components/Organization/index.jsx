b0141a6a87ff2ea02b468f79900c657b08a50d13
Janette Day <janette.day@civicactions.com>
2020-09-25 09:52:42 -0500

    use key prop on organization rather than identifier

--- src/components/Organization/index.jsx
+++ src/components/Organization/index.jsx
@@ -3,14 +3,14 @@ import PropTypes from "prop-types";
 import { Link } from "@reach/router";
 
 function Organization(props) {
-  const { name, description, identifier, imageUrl, searchUrl } = props;
+  const { name, description, key, imageUrl, searchUrl } = props;
   const image = <img alt={name || "Organization Image"} src={imageUrl} />;
   const alignment = props.alignment ? props.alignment : 'center';
   const link = searchUrl ? searchUrl : `search/?publisher__name=${name}`;
 
   return (
     <div className="dc-org-block" style={{ textAlign: alignment }}>
-      <Link to={link} className="dc-org-image" alt="Organization Logo">
+      <Link to={link} className="dc-org-image" alt="Organization Logo" key={key}>
         {image}
         <h3 className="dc-org-name">
           {name}
@@ -28,7 +28,7 @@ function Organization(props) {
 Organization.defaultProps = {
   name: "",
   description: "",
-  identifier: "",
+  key: "",
   imageUrl:
     "https://s3.amazonaws.com/dkan-default-content-files/files/group.png"
 };
@@ -36,7 +36,7 @@ Organization.defaultProps = {
 Organization.propTypes = {
   name: PropTypes.string,
   description: PropTypes.string,
-  identifier: PropTypes.string,
+  key: PropTypes.string,
   imageUrl: PropTypes.string,
   searchUrl: PropTypes.string
 };
