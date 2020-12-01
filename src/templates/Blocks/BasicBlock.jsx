cccb8171211105a951f87dd3d4ac7bc5750ba42b
Janette Day <janette.day@civicactions.com>
2020-12-01 16:53:05 -0600

    add image prop to BasicBlock template

--- src/templates/Blocks/BasicBlock.jsx
+++ src/templates/Blocks/BasicBlock.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from '@reach/router';
 import Text from '../../components/Text';
 
 class BasicBlock extends React.PureComponent {
   render() {
     const { content } = this.props;
+    const img = content.image ? <Link to={content.ref}><img alt="" src={content.image} /></Link> : null;
 
     return (
       <div key={content.ref} className="basic-block">
-        <h2>{content.title}</h2>
+        {img}
+        <h2><Link to={content.ref}>{content.title}</Link></h2>
         <Text value={content.content} />
       </div>
     );
@@ -18,11 +21,13 @@ class BasicBlock extends React.PureComponent {
 BasicBlock.defaultProps = {
   title: '',
   content: '',
+  image: '',
 };
 
 BasicBlock.propTypes = {
   title: PropTypes.string,
   content: PropTypes.string,
+  image: PropTypes.string,
 };
 
 export default BasicBlock;
