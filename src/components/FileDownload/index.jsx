674f6f8f8122ffc240e6f6aa8ba6a816466eddb2
Janette Day <janette.day@civicactions.com>
2020-12-01 16:56:40 -0600

    add accessURL prop to FileDownload

--- src/components/FileDownload/index.jsx
+++ src/components/FileDownload/index.jsx
@@ -4,13 +4,14 @@ import FormatIcon from '../FormatIcon';
 import Text from '../Text';
 
 const FileDownload = ({
-  title, format, downloadURL, description,
+  title, format, downloadURL, accessURL, description,
 }) => {
   const label = title || format;
+  const ref = downloadURL ? downloadURL : accessURL;
   const item = (
     <div className="dc-resource">
       <FormatIcon format={format} />
-      <a href={downloadURL} title={label}>
+      <a href={ref} title={label}>
         {label}
       </a>
       {description && (
@@ -29,12 +30,15 @@ const FileDownload = ({
 FileDownload.defaultProps = {
   description: '',
   title: '',
+  downloadURL: '',
+  accessURL: '',
 };
 
 FileDownload.propTypes = {
   title: PropTypes.string,
   format: PropTypes.string.isRequired,
-  downloadURL: PropTypes.string.isRequired,
+  downloadURL: PropTypes.string,
+  accessURL: PropTypes.string,
   description: PropTypes.string,
 };
 
