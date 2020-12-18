88e8bd04022eed023dd97ca1ea779addea6a4328
Janette Day <janette.day@civicactions.com>
2020-12-18 16:08:57 -0600

    add image prop to BasicBlock template (#104)
    * add image prop to BasicBlock template
    
    * Avoid redundant links
    
    * Update BasicBlock
    
    * fix propTypes and defaultProps
    
    * fix import

--- src/templates/Blocks/BasicBlock.jsx
+++ src/templates/Blocks/BasicBlock.jsx
@@ -1,28 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from '@reach/router';
 import Text from '../../components/Text';
 
 class BasicBlock extends React.PureComponent {
   render() {
     const { content } = this.props;
+    const img = content.image ? <div><img alt="" src={content.image} /></div> : null;
+
+    let block = '';
+
+    if (content.ref) {
+      block = (
+        <div key={content.ref} className="basic-block">
+          <h2>
+            <Link to={content.ref}>
+              {img}
+              {content.title}
+            </Link>
+          </h2>
+          <Text value={content.teaser} />
+        </div>
+      );
+    } else {
+      block = (
+        <div key={content.title} className="basic-block">
+          <h2>
+            {img}
+            {content.title}
+          </h2>
+          <Text value={content.teaser} />
+        </div>
+      );
+    }
 
     return (
-      <div key={content.ref} className="basic-block">
-        <h2>{content.title}</h2>
-        <Text value={content.content} />
-      </div>
+      <>
+        {block}
+      </>
     );
   }
 }
 
 BasicBlock.defaultProps = {
-  title: '',
-  content: '',
+  content: [
+    {
+      title: '',
+      content: '',
+      image: '',
+      teaser: '',
+      ref: '',
+    },
+  ],
 };
 
 BasicBlock.propTypes = {
-  title: PropTypes.string,
-  content: PropTypes.string,
+  content: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    teaser: PropTypes.string,
+    image: PropTypes.string,
+    ref: PropTypes.string,
+  })),
 };
 
 export default BasicBlock;
