e9d67f2a416868f70a2f19f01b947d13a7c898da
Alex Scott <alex.scott@civicactions.com>
2021-03-22 13:19:21 +0100

    Tests for counting datasets within and Organization.

--- src/components/Organization/index.jsx
+++ src/components/Organization/index.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import PropTypes from "prop-types";
 import { Link } from "@reach/router";
+import PublisherDatasetCountByName from "../PublisherDatasetCountByName";
+import axios from 'axios';
 
 function Organization(props) {
-  const { name, description, imageUrl, searchUrl, alignment } = props;
+  const { name, description, imageUrl, searchUrl, alignment, datasetCount } = props;
   const image = <img alt={name || 'Organization Image'} src={imageUrl} />;
   const link = searchUrl ? searchUrl : `search/?publisher__name=${name}`;
+
+  const [posts, setPosts] = useState();
+
+  const fetchData = async () => {
+    const { data } = await axios.get('http://demo.getdkan.org/data.json');
+    console.log(data);
+    console.log("Name", name);
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="dc-org-block" style={{ textAlign: alignment }}>
@@ -20,9 +35,25 @@ function Organization(props) {
           {description}
         </div>
       )}
+
+      {posts && posts.dataset !== 'undefined' ? countDatasetsByName("State Economic Council", posts.dataset) : null}
+
+      <PublisherDatasetCountByName datasetCount={datasetCount} />
     </div>
   );
 }
+
+export const countDatasetsByName = (publisher, datasets) =>  {
+  // console.log("datasets", datasets);
+  // return "foobar";
+  const publishers = datasets.map((data, index, arr) => {return data.publisher; });
+  const result =  publishers.filter((p) => {return p.name === publisher;})
+   console.log("RES: ", result);
+  if (typeof result !== 'undefined' && result.length) {
+    return result.length;
+  }
+  return null;
+};
 
 Organization.defaultProps = {
   alignment: "center",
@@ -33,6 +64,7 @@ Organization.defaultProps = {
 
 Organization.propTypes = {
   alignment: PropTypes.string,
+  datasetCount: PropTypes.string,
   name: PropTypes.string,
   description: PropTypes.string,
   imageUrl: PropTypes.string,
--- src/components/Organization/index.test.jsx
+++ src/components/Organization/index.test.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Organization from './index';
+import Organization, {countDatasetsByName} from './index';
+import PublisherDatasetCountByName from '../PublisherDatasetCountByName';
+
+const data =
+      [{"publisher": {
+        "@type": "org:Organization",
+        "name": "State Economic Council"
+      }},
+      {"publisher": {
+        "@type": "org:Organization",
+        "name": "State Economic Council"
+      }}];
+
 
 describe('<Organization />', () => {
   test('renders a heading', () => {
     render(<Organization name="DKAN" />);
     expect(screen.getByRole('heading', 'DKAN')).toBeInTheDocument();
   });
+
+  test('Has a publisher name.', () => {
+    expect(data[0]['publisher']['name']).toEqual("State Economic Council");
+  });
+
+  test('renders with a dataset link with no count', () => {
+    render(<Organization name="DKAN" />);
+    expect(screen.getByText('datasets')).toBeInTheDocument();
+  });
+
+  test('Calculates a count from data',() => {
+    expect(countDatasetsByName("State Economic Council", data)).toEqual(2);
+  });
+
 });
