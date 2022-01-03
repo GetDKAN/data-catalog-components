f4acc0d497f5aeb69cc247e2bc035a286d32b128
dgading <dgading@gmail.com>
2022-01-03 13:55:12 -0500

    Remove APIDocs component in favor of downstream instances

--- stories/general.stories.jsx
+++ stories/general.stories.jsx
@@ -1,43 +1,70 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withKnobs, text, select, boolean, array, number } from '@storybook/addon-knobs';
-import { withA11y } from '@storybook/addon-a11y';
-import Header from '../src/templates/Header';
-import NavBar from '../src/templates/NavBar';
-import Menu from '../src/components/Menu';
-import PageHeader from '../src/components/PageHeader';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import DataIcon from '../src/components/DataIcon';
-import Footer from '../src/templates/Footer';
-import ApiDocs from '../src/components/ApiDocs';
-import ToggleBlock from '../src/components/ToggleBlock';
-import ShowMoreContainer from '../src/components/ShowMoreContainer';
-import Announcement from '../src/templates/Announcement';
-import PublisherList from '../src/components/PublisherList';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import {
+  withKnobs,
+  text,
+  select,
+  boolean,
+  array,
+  number,
+} from "@storybook/addon-knobs";
+import { withA11y } from "@storybook/addon-a11y";
+import Header from "../src/templates/Header";
+import NavBar from "../src/templates/NavBar";
+import Menu from "../src/components/Menu";
+import PageHeader from "../src/components/PageHeader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import DataIcon from "../src/components/DataIcon";
+import Footer from "../src/templates/Footer";
+import ToggleBlock from "../src/components/ToggleBlock";
+import ShowMoreContainer from "../src/components/ShowMoreContainer";
+import Announcement from "../src/templates/Announcement";
+import PublisherList from "../src/components/PublisherList";
 import "../src/theme/styles/index.scss";
 
-import links from './data/menu.json';
-import orgs from './data/publishers.json';
+import links from "./data/menu.json";
+import orgs from "./data/publishers.json";
 
-storiesOf('General', module)
+storiesOf("General", module)
   .addDecorator(withKnobs)
   .addDecorator(withA11y)
-  .add('Header', () =>
+  .add("Header", () => (
     <Header
       site="Open Data Catalog"
       slogan="Place your tag line here."
       customClasses="container-fluid"
-      navItems={links.main.map((item) => (<a href={item.url}>{item.label}</a>))}
+      navItems={links.main.map((item) => (
+        <a href={item.url}>{item.label}</a>
+      ))}
     />
-  )
-  .add('NavBar', () => <NavBar navItems={links.main.map((item) => (<a href={item.url}>{item.label}</a>))} customClasses="container-fluid" />)
-  .add('Menu (external links)', () => <Menu items={links.footer1} title={text("Title", "External links")} menuId="sampleNav"/>)
-  .add('Menu (internal links', () => <Menu items={links.main} title={text("Title", "Internal links")} menuId="sampleNav"/>)
-  .add('Page Heading', () => <PageHeader title="Datasets" />)
-  .add('ToggleBlock', () => (
+  ))
+  .add("NavBar", () => (
+    <NavBar
+      navItems={links.main.map((item) => (
+        <a href={item.url}>{item.label}</a>
+      ))}
+      customClasses="container-fluid"
+    />
+  ))
+  .add("Menu (external links)", () => (
+    <Menu
+      items={links.footer1}
+      title={text("Title", "External links")}
+      menuId="sampleNav"
+    />
+  ))
+  .add("Menu (internal links", () => (
+    <Menu
+      items={links.main}
+      title={text("Title", "Internal links")}
+      menuId="sampleNav"
+    />
+  ))
+  .add("Page Heading", () => <PageHeader title="Datasets" />)
+  .add("ToggleBlock", () => (
     <ToggleBlock
-      title={text('Title', 'Block Title')}
-      allowToggle={boolean('Toggle Header Button', true)}
+      title={text("Title", "Block Title")}
+      allowToggle={boolean("Toggle Header Button", true)}
     >
       <p>Inner content.</p>
       <ul>
@@ -47,17 +74,35 @@ storiesOf('General', module)
       </ul>
     </ToggleBlock>
   ))
-  .add('Show More Container', () => {
-    const itemsArray = ['open', 'data', 'dkan', 'test', 'React', 'Drupal', 'foo', 'bar', 'JavaScript', 'PHP', 'source'];
+  .add("Show More Container", () => {
+    const itemsArray = [
+      "open",
+      "data",
+      "dkan",
+      "test",
+      "React",
+      "Drupal",
+      "foo",
+      "bar",
+      "JavaScript",
+      "PHP",
+      "source",
+    ];
     return (
       <ShowMoreContainer
-        container={select('Container Type', { ul: 'ul', ol: 'ol', div: 'div' }, 'ol')}
-        items={array('Show More Items', itemsArray).map((item) => (<li key={item}>{item}</li>))}
-        limit={number('Limit', 10)}
-        btnOpenText={text('Label when open', '')}
-        btnClosedText={text('Label when closed', '')}
+        container={select(
+          "Container Type",
+          { ul: "ul", ol: "ol", div: "div" },
+          "ol"
+        )}
+        items={array("Show More Items", itemsArray).map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+        limit={number("Limit", 10)}
+        btnOpenText={text("Label when open", "")}
+        btnClosedText={text("Label when closed", "")}
       />
-    )
+    );
   })
   .add(
     "Data Icon",
@@ -68,11 +113,10 @@ storiesOf('General', module)
           ["density-1", "density-2", "density-3", "group"],
           "density-1"
         )}
-        color={text('Color', "#41b0d3")}
+        color={text("Color", "#41b0d3")}
         height={text("Height", "50")}
         width={text("Width", "50")}
-      >
-      </DataIcon>
+      ></DataIcon>
     ),
     { knobs: { escapeHTML: false } }
   )
@@ -85,36 +129,25 @@ storiesOf('General', module)
   ))
   .add("FontAwesome Brand Icon", () => (
     <span>
-      <FontAwesomeIcon
-        icon={['fab', 'facebook']}
-        color="blue"
-        size="4x"
-      />
-      <FontAwesomeIcon
-        icon={['fab', 'twitter']}
-        color="aqua"
-        size="4x"
-      />
-      <FontAwesomeIcon
-        icon={['fab', 'github']}
-        color="purple"
-        size="4x"
-      />
+      <FontAwesomeIcon icon={["fab", "facebook"]} color="blue" size="4x" />
+      <FontAwesomeIcon icon={["fab", "twitter"]} color="aqua" size="4x" />
+      <FontAwesomeIcon icon={["fab", "github"]} color="purple" size="4x" />
     </span>
   ))
-  .add('Footer', () => <Footer links={links} customClasses="container-fluid" />)
-  .add('API Documentation', () => <ApiDocs endpoint="https://petstore.swagger.io/v2/swagger.json" />)
-  .add('Announcement', () => (
+  .add("Footer", () => <Footer links={links} customClasses="container-fluid" />)
+  .add("API Documentation", () => (
+    <ApiDocs endpoint="https://petstore.swagger.io/v2/swagger.json" />
+  ))
+  .add("Announcement", () => (
     <Announcement
-      heading={text('Heading', "Note")}
+      heading={text("Heading", "Note")}
       variation={select(
         "Variation",
-        ["","info", "success", "warn", "error"],
+        ["", "info", "success", "warn", "error"],
         "info"
-      )}>
+      )}
+    >
       Pay attention this is important information.
     </Announcement>
   ))
-  .add('Publisher List', () => (
-    <PublisherList items={orgs} className="test" />
-  ))
+  .add("Publisher List", () => <PublisherList items={orgs} className="test" />);
