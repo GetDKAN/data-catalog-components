75b5e82871a0f91de029b0b795d8655e641e1f21
Daniel <dgading@gmail.com>
2022-01-03 23:22:06 -0500

    Remove APIDocs component in favor of downstream instances (#160)

--- src/index.js
+++ src/index.js
@@ -1,40 +1,37 @@
-export { default as ApiDocs } from './components/ApiDocs';
-export { default as Card } from './components/Card';
-export { default as DataIcon } from './components/DataIcon';
-export { default as DataTableDensity } from './components/DataTable/DataTableDensity';
-export { default as DataTablePageResults } from './components/DataTable/DataTablePageResults';
-export { default as DataTablePageSizer } from './components/DataTable/DataTablePageSizer';
-export { default as DynamicLink } from './components/DynamicLink';
-export { default as IconList } from './components/IconList';
-export { default as IconListItem } from './components/IconListItem';
-export { default as FileDownload } from './components/FileDownload';
-export { default as FormatIcon } from './components/FormatIcon';
-export { default as ManageColumns } from './components/DataTable/ManageColumns';
-export { default as Menu } from './components/Menu';
-export { default as Modal } from './components/Modal';
-export { default as NavBar } from './templates/NavBar';
-export { default as Organization } from './components/Organization';
-export { default as ShowMoreContainer } from './components/ShowMoreContainer';
-export { default as Tags } from './components/Tags';
-export { default as TopicWrapper } from './components/TopicWrapper';
+export { default as Card } from "./components/Card";
+export { default as DataIcon } from "./components/DataIcon";
+export { default as DataTableDensity } from "./components/DataTable/DataTableDensity";
+export { default as DataTablePageResults } from "./components/DataTable/DataTablePageResults";
+export { default as DataTablePageSizer } from "./components/DataTable/DataTablePageSizer";
+export { default as DynamicLink } from "./components/DynamicLink";
+export { default as IconList } from "./components/IconList";
+export { default as IconListItem } from "./components/IconListItem";
+export { default as FileDownload } from "./components/FileDownload";
+export { default as FormatIcon } from "./components/FormatIcon";
+export { default as ManageColumns } from "./components/DataTable/ManageColumns";
+export { default as Menu } from "./components/Menu";
+export { default as Modal } from "./components/Modal";
+export { default as NavBar } from "./templates/NavBar";
+export { default as Organization } from "./components/Organization";
+export { default as ShowMoreContainer } from "./components/ShowMoreContainer";
+export { default as Tags } from "./components/Tags";
+export { default as TopicWrapper } from "./components/TopicWrapper";
 
 // TEMPLATES
-export { default as Blocks } from './templates/Blocks';
-export { default as BasicBlock } from './templates/Blocks/BasicBlock';
-export { default as Header } from './templates/Header';
-export { default as Hero } from './templates/Hero';
-export { default as Footer } from './templates/Footer';
-export { default as PublisherList } from './templates/PublisherList';
-export { default as StatBlock } from './templates/Blocks/StatBlock';
-export { default as StepsBlock } from './templates/Blocks/StepsBlock';
-export { default as TopicIcon } from './templates/TopicIcon';
-export { default as DataTableHeader } from './templates/DataTableHeader';
+export { default as Blocks } from "./templates/Blocks";
+export { default as BasicBlock } from "./templates/Blocks/BasicBlock";
+export { default as Header } from "./templates/Header";
+export { default as Hero } from "./templates/Hero";
+export { default as Footer } from "./templates/Footer";
+export { default as PublisherList } from "./templates/PublisherList";
+export { default as StatBlock } from "./templates/Blocks/StatBlock";
+export { default as StepsBlock } from "./templates/Blocks/StepsBlock";
+export { default as TopicIcon } from "./templates/TopicIcon";
+export { default as DataTableHeader } from "./templates/DataTableHeader";
 
+export { default as DataTable } from "./components/DataTable";
 
-export { default as DataTable } from './components/DataTable';
-
-
-export { default as DatasetSearchCards } from './components/DatasetSearch/DatasetSearchCards';
-export { default as DatasetSearchFulltext } from './components/DatasetSearch/DatasetSearchFulltext';
-export { default as DatasetSearchSort } from './components/DatasetSearch/DatasetSearchSort';
-export { default as DatasetSearchFacetBlock } from './components/DatasetSearch/DatasetSearchFacetBlock';
+export { default as DatasetSearchCards } from "./components/DatasetSearch/DatasetSearchCards";
+export { default as DatasetSearchFulltext } from "./components/DatasetSearch/DatasetSearchFulltext";
+export { default as DatasetSearchSort } from "./components/DatasetSearch/DatasetSearchSort";
+export { default as DatasetSearchFacetBlock } from "./components/DatasetSearch/DatasetSearchFacetBlock";
