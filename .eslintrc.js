7a414112427977222f9d92878ccd6b4f05e07fb3
Alex Ahumada <alejandro.ahumadaperez@cadiz.es>
2022-04-05 10:39:23 +0200

    feat: Update Babel, ESLint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,16 @@
 module.exports = {
-    "extends": "airbnb",
-    "parser": "babel-eslint",
-    "rules": {
-        "react/jsx-curly-spacing": [2, "never", { "allowMultiline": true }],
-    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:storybook/recommended",
+        "plugin:prettier/recommended"
+    ],
+    "parser": "@babel/eslint-parser",
+    "plugins": ["testing-library"],
     "env": {
         "jest": true,
         "browser": true,
+    },
+    "rules": {
+        "react/jsx-curly-spacing": [2, "never", { "allowMultiline": true }],
     }
 };
