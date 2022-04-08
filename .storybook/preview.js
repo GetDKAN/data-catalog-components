0d2bb24427dbbbb911cd37affdf8460098ff74be
Alex Ahumada <alejandro.ahumadaperez@cadiz.es>
2022-04-08 14:52:23 +0200

    Add controls and sort to Storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,17 @@ import '../src/styles/scss/index.scss';
 
 export const parameters = {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+        matchers: {
+            color: /(background|color|fill)$/i,
+            date: /Date$/,
+        },
+    },
+    options: {
+        storySort: {
+            method: 'alphabetical',
+            order: ['Home', 'Components', 'Services', 'Templates', '*', 'WIP'],
+            locales: 'en-US',
+        },
+    },
 }
