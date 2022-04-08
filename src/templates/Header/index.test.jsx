c1bc79b37f35a591f1c0480dc21b2c40b9090b4c
Alex Ahumada <alejandro.ahumadaperez@cadiz.es>
2022-04-08 15:02:12 +0200

    Add Header props to tests to avoid console errors

--- src/templates/Header/index.test.jsx
+++ src/templates/Header/index.test.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Header from './index';
+import menu from '../../examples/menu.json';
 
 describe('<Header />', () => {
   test('renders a logo by default', () => {
-    render(<Header />);
+    render(
+      <Header
+        navItems={menu.map((item) => (
+          <a href={item.url}>{item.label}</a>
+        ))}
+      />
+    );
     expect(screen.getByAltText('Open Data Catalog')).toBeInTheDocument();
   });
 });
