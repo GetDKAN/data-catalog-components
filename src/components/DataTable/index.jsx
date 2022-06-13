05a637574ac60f67ef6e02ef77c75e9d494058d3
Daniel <dgading@gmail.com>
2022-06-13 13:19:37 -0400

    Remove all unused and out of date components

--- src/components/DataTable/index.jsx
+++ src/components/DataTable/index.jsx
@@ -1,6 +1,14 @@
-import React, {useEffect} from 'react';
-import PropTypes from 'prop-types';
-import { useTable, usePagination, useResizeColumns, useSortBy, useFilters, useFlexLayout, useBlockLayout } from 'react-table';
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import {
+  useTable,
+  usePagination,
+  useResizeColumns,
+  useSortBy,
+  useFilters,
+  useFlexLayout,
+  useBlockLayout,
+} from "react-table";
 
 const DataTable = ({
   data,
@@ -18,12 +26,12 @@ const DataTable = ({
   className,
   tableClasses,
   customColumnFilter,
-  cellTextClassName, 
+  cellTextClassName,
   CustomLoadingComponent,
   CustomNoResults,
 }) => {
   const { layout, columnFilter, columnSort, columnResize } = options;
-  const { minWidth, maxWidth, width } = columnDefaults
+  const { minWidth, maxWidth, width } = columnDefaults;
   const {
     tableContainerClassName,
     headerCellClassName,
@@ -41,9 +49,9 @@ const DataTable = ({
     headerFilterCellClassName,
     columnIsSortedClassName,
     columnIsSortedAscClassName,
-    columnIsSortedDecClassName
+    columnIsSortedDecClassName,
   } = tableClasses;
-  if(columns.length === 0) {
+  if (columns.length === 0) {
     return <p>Loading</p>;
   }
 
@@ -53,18 +61,17 @@ const DataTable = ({
       // fuzzyText: fuzzyTextFilterFn,
       // Or, override the default text filter to use
       // "startWith"
-      text: (rows, id, filterValue) => (
+      text: (rows, id, filterValue) =>
         rows.filter((row) => {
           const rowValue = row.values[id];
           return rowValue !== undefined
             ? String(rowValue)
-              .toLowerCase()
-              .startsWith(String(filterValue).toLowerCase())
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
             : true;
-        })
-      ),
+        }),
     }),
-    [],
+    []
   );
 
   function DefaultColumnFilter({
@@ -76,32 +83,32 @@ const DataTable = ({
           className={defaultColumnFilterClassName}
           htmlFor={`filter_${accessor}`}
         >
-            {`Filter ${Header}`}
+          {`Filter ${Header}`}
         </label>
         <input
           className="usa-input margin-bottom-1"
-          onChange={(e) => { setFilter(e.target.value) || undefined }}
+          onChange={(e) => {
+            setFilter(e.target.value) || undefined;
+          }}
           id={`filter_${accessor}`}
           name={`filter_${accessor}`}
           type="text"
-          value={filterValue || ''}
+          value={filterValue || ""}
         />
       </div>
     );
   }
 
-   
-
   const defaultColumn = React.useMemo(
     () => ({
       // Let's set up our default Filter UI
-      Filter: customColumnFilter ? customColumnFilter :DefaultColumnFilter,
+      Filter: customColumnFilter ? customColumnFilter : DefaultColumnFilter,
       // When using the useFlexLayout:
       minWidth: minWidth, // minWidth is only used as a limit for resizing
       width: width, // width is used for both the flex-basis and flex-grow
       maxWidth: maxWidth, // maxWidth is only used as a limit for resizing
     }),
-    [],
+    []
   );
 
   const {
@@ -115,7 +122,7 @@ const DataTable = ({
       columns,
       data,
       filterTypes,
-      initialState: {  },
+      initialState: {},
       manualPagination: true,
       manualFilters: true,
       manualSortBy: true,
@@ -127,173 +134,170 @@ const DataTable = ({
     useFilters,
     useSortBy,
     usePagination,
-    (layout === 'block') ? useBlockLayout : useFlexLayout,
+    layout === "block" ? useBlockLayout : useFlexLayout
   );
 
   useEffect(() => {
-    if(columnSort) {
+    if (columnSort) {
       const normalizedSort = sortTransform ? sortTransform(sortBy) : filters;
       setSort(normalizedSort);
     }
-  }, [sortBy])
+  }, [sortBy]);
 
   useEffect(() => {
     let timerFunc = setTimeout(() => {
-      if(columnFilter) {
-        const normalizedFilters = conditionsTransform ? conditionsTransform(filters) : filters;
-        setConditions(normalizedFilters)
+      if (columnFilter) {
+        const normalizedFilters = conditionsTransform
+          ? conditionsTransform(filters)
+          : filters;
+        setConditions(normalizedFilters);
       }
     }, 1000);
     return () => clearTimeout(timerFunc);
-  }, [filters])
+  }, [filters]);
 
-  return(
-    <div {...getTableProps()}
-      className={className}
-    >
+  return (
+    <div {...getTableProps()} className={className}>
       <div className={tableContainerClassName}>
-        {headerGroups.map(headerGroup => (
+        {headerGroups.map((headerGroup) => (
           <div
             {...headerGroup.getHeaderGroupProps()}
             className={headerGroupClassName}
           >
-            {headerGroup.headers.map(column => (
-              <> 
+            {headerGroup.headers.map((column) => (
+              <>
                 <div
                   className={headerCellClassName}
-                  {...column.getHeaderProps(columnSort ? column.getSortByToggleProps() : undefined)}
+                  {...column.getHeaderProps(
+                    columnSort ? column.getSortByToggleProps() : undefined
+                  )}
                 >
                   <span className={headerCellTextClassName}>
-                    {column.render('Header')}
+                    {column.render("Header")}
                   </span>
-                  {columnSort
-                      && (
-                        <span
-                          className={
-                            column.isSorted
-                            ? column.isSortedDesc
-                              ? columnIsSortedDecClassName
-                              : columnIsSortedAscClassName
-                            : columnIsSortedClassName
-                          }
-                        />
-                      )
-                    }
-                </div>
-                {columnResize
-                  &&(
-                    <div
-                      {...column.getResizerProps()}
-                      className={`${tableColumnResizer} ${
-                        column.isResizing ? tableColumnIsResizing : ''
-                      }`}
+                  {columnSort && (
+                    <span
+                      className={
+                        column.isSorted
+                          ? column.isSortedDesc
+                            ? columnIsSortedDecClassName
+                            : columnIsSortedAscClassName
+                          : columnIsSortedClassName
+                      }
                     />
-                  )
-                }
+                  )}
+                </div>
+                {columnResize && (
+                  <div
+                    {...column.getResizerProps()}
+                    className={`${tableColumnResizer} ${
+                      column.isResizing ? tableColumnIsResizing : ""
+                    }`}
+                  />
+                )}
               </>
             ))}
-            
           </div>
         ))}
-        {columnFilter
-          &&(
-            <div className={headerFilterClassName}>
-               {filterTitle
-                &&(
-                  <span className={filterTitleClassName}>
-                    {filterTitle}
-                  </span>
-                )
-              }
-              {headerGroups.map((headerGroup) => (
-                <div
-                  {...headerGroup.getHeaderGroupProps()}
-                >
-                 
-                  {headerGroup.headers.map(column => (
-                    <div
-                      {...column.getHeaderProps()}
-                      className={headerFilterCellClassName}
-                    > 
-                      <div className="th">
-                        <div>{column.canFilter ? column.render('Filter') : null}</div>
+        {columnFilter && (
+          <div className={headerFilterClassName}>
+            {filterTitle && (
+              <span className={filterTitleClassName}>{filterTitle}</span>
+            )}
+            {headerGroups.map((headerGroup) => (
+              <div {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <div
+                    {...column.getHeaderProps()}
+                    className={headerFilterCellClassName}
+                  >
+                    <div className="th">
+                      <div>
+                        {column.canFilter ? column.render("Filter") : null}
                       </div>
-                      
                     </div>
-                  ))}
-                </div>
-              ))}
-            </div>
-          )}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
       <div className="tbody">
-        {loading
-          ? (CustomLoadingComponent ? CustomLoadingComponent : <p>Loading...</p>)
-          : (
-            rows.length
-              ? (
-                rows.map((row, index) => {
-                  const even = (index + 1) % 2 === 0;
-                  prepareRow(row)
-                  let classList = cellClassName
-                  if (index === 0 && cellFirstRowClassName) {
-                    classList = cellFirstRowClassName
-                  }
-                  if (even) {
-                    classList += ` ${cellEvenRowClassName}`
-                  }
-                  if (!even) {
-                    classList += ` ${cellOddRowClassName}`
-                  }
-                  return (
-                    <div {...row.getRowProps()} className="tr">
-                      {row.cells.map((cell) => {
-                        return (
-                          <div {...cell.getCellProps()} className={classList}>
-                            <span className={cellTextClassName}>
-                              {cell.render('Cell')}
-                            </span>
-                          </div>
-                        )
-                      })}
-                    </div>
-                  )
-                })
-              ) : (
-                CustomNoResults ? CustomNoResults : <p>No results found.</p>
-              )
+        {loading ? (
+          CustomLoadingComponent ? (
+            CustomLoadingComponent
+          ) : (
+            <p>Loading...</p>
           )
-        }
+        ) : rows.length ? (
+          rows.map((row, index) => {
+            const even = (index + 1) % 2 === 0;
+            prepareRow(row);
+            let classList = cellClassName;
+            if (index === 0 && cellFirstRowClassName) {
+              classList = cellFirstRowClassName;
+            }
+            if (even) {
+              classList += ` ${cellEvenRowClassName}`;
+            }
+            if (!even) {
+              classList += ` ${cellOddRowClassName}`;
+            }
+            return (
+              <div {...row.getRowProps()} className="tr">
+                {row.cells.map((cell) => {
+                  return (
+                    <div {...cell.getCellProps()} className={classList}>
+                      <span className={cellTextClassName}>
+                        {cell.render("Cell")}
+                      </span>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })
+        ) : CustomNoResults ? (
+          CustomNoResults
+        ) : (
+          <p>No results found.</p>
+        )}
       </div>
     </div>
-  )
+  );
 };
 
 DataTable.defaultProps = {
-  filterTitle: '',
-  className: '',
+  filterTitle: "",
+  className: "",
   tableClasses: {
-    tableContainerClassName: '',
-    tableColumnResizer: 'width-1 bg-primary margin-right-neg-05 position-absolute top-0 bottom-0 right-0 z-100',
-    tableColumnIsResizing: '',
-    headerCellClassName: 'bg-base-lighter border-top-1px text-right text-bold padding-x-1 overflow-hidden',
-    headerGroupClassName: '',
-    headerCellTextClassName: 'overflow-hidden text-no-wrap display-inline-block',
-    defaultColumnFilterClassName: 'usa-label usa-sr-only',
-    cellTextClassName: 'overflow-hidden text-no-wrap display-inline-block',
-    cellFirstRowClassName: '',
-    cellClassName: 'border-right-1px font-mono-sm text-tabular text-right padding-x-105 padding-y-05',
-    cellEvenRowClassName: 'bg-base-lightest',
-    cellOddRowClassName: '',
-    filterTitleClassName: '',
-    headerFilterCellClassName: 'bg-base-lighter border-bottom-1px border-right-1px padding-x-1',
-    headerFilterClassName: '',
-    columnIsSortedClassName: '',
-    columnIsSortedAscClassName: '',
-    columnIsSortedDecClassName: '',
+    tableContainerClassName: "",
+    tableColumnResizer:
+      "width-1 bg-primary margin-right-neg-05 position-absolute top-0 bottom-0 right-0 z-100",
+    tableColumnIsResizing: "",
+    headerCellClassName:
+      "bg-base-lighter border-top-1px text-right text-bold padding-x-1 overflow-hidden",
+    headerGroupClassName: "",
+    headerCellTextClassName:
+      "overflow-hidden text-no-wrap display-inline-block",
+    defaultColumnFilterClassName: "usa-label usa-sr-only",
+    cellTextClassName: "overflow-hidden text-no-wrap display-inline-block",
+    cellFirstRowClassName: "",
+    cellClassName:
+      "border-right-1px font-mono-sm text-tabular text-right padding-x-105 padding-y-05",
+    cellEvenRowClassName: "bg-base-lightest",
+    cellOddRowClassName: "",
+    filterTitleClassName: "",
+    headerFilterCellClassName:
+      "bg-base-lighter border-bottom-1px border-right-1px padding-x-1",
+    headerFilterClassName: "",
+    columnIsSortedClassName: "",
+    columnIsSortedAscClassName: "",
+    columnIsSortedDecClassName: "",
   },
   options: {
-    layout: 'flex',
+    layout: "flex",
     columnFilter: true,
     columnSort: true,
     columnResize: true,
@@ -304,19 +308,21 @@ DataTable.defaultProps = {
     width: 150,
   },
   customColumnFilter: undefined,
-}
+};
 
 DataTable.propTypes = {
   options: PropTypes.shape({
-    layout: PropTypes.oneOf(['flex', 'block']),
+    layout: PropTypes.oneOf(["flex", "block"]),
     columnFilter: PropTypes.bool,
     columnSort: PropTypes.bool,
     columnResize: PropTypes.bool,
   }),
-  columns: PropTypes.arrayOf(PropTypes.shape({
-    Header: PropTypes.string.isRequired,
-    accessor: PropTypes.string.isRequired,
-  })).isRequired,
+  columns: PropTypes.arrayOf(
+    PropTypes.shape({
+      Header: PropTypes.string.isRequired,
+      accessor: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   setSort: PropTypes.func.isRequired,
   setConditions: PropTypes.func.isRequired,
   conditionsTransform: PropTypes.func.isRequired,
@@ -327,14 +333,16 @@ DataTable.propTypes = {
   totalRows: PropTypes.number.isRequired,
   limit: PropTypes.number.isRequired,
   loading: PropTypes.bool,
-  columnDefaults: PropTypes.arrayOf(PropTypes.shape({
-    minWidth: PropTypes.number,
-    maxWidth: PropTypes.number,
-    width: PropTypes.number,
-  })),
+  columnDefaults: PropTypes.arrayOf(
+    PropTypes.shape({
+      minWidth: PropTypes.number,
+      maxWidth: PropTypes.number,
+      width: PropTypes.number,
+    })
+  ),
   customColumnFilter: PropTypes.func,
   // tableClasses,
   // cellTextClassName
-}
+};
 
 export default DataTable;
