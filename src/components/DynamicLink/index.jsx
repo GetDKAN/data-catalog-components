2b73b717c513ef51e53ff2ff644a9661f229a538
Daniel <dgading@gmail.com>
2022-06-14 10:36:08 -0400

    Remove all unused and out of date components (#167)

--- src/components/DynamicLink/index.jsx
+++ src/components/DynamicLink/index.jsx
@@ -1,44 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from '@reach/router';
-import validator from 'validator';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "@reach/router";
+import validator from "validator";
 
-const DynamicLink = ({url, content, cue, className}) => {
-  if(validator.isURL(url, { require_protocol: true })) {
+const DynamicLink = ({ url, content, cue, className }) => {
+  if (validator.isURL(url, { require_protocol: true })) {
     return (
-      <a
-        className={className}
-        href={url}
-      >
+      <a className={className} href={url}>
         {content}
-        {cue
-          && (
-            cue
-          )
-        }
+        {cue && cue}
       </a>
-    )
+    );
   }
-  return(
-    <Link
-      className={className}
-      to={url}
-    >
+  return (
+    <Link className={className} to={url}>
       {content}
     </Link>
   );
-}
+};
 
 DynamicLink.defaultProps = {
   cue: undefined,
-  className: '',
-}
+  className: "",
+};
 
 DynamicLink.propTypes = {
   url: PropTypes.string.isRequired,
   content: PropTypes.node.isRequired,
   cue: PropTypes.node,
   className: PropTypes.string,
-}
+};
 
 export default DynamicLink;
